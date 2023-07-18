956be5605ebf6a4c7df20867fef324860ba21589
Yoga Vredizon <yogavredizon7@gmail.com>
2023-07-18 19:52:53 +0700

    commit kedua

---
+++ Programming/Golang/Go-Get/go.mod
@@ -0,0 +1,5 @@
+module github.com/yogavredizon/go-get
+
+go 1.19
+
+require github.com/yogavredizon/go-modules v0.0.0-20230718122524-4f6d0418db89 // indirect
---
+++ Programming/Golang/Go-Get/main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+
+	m "github.com/yogavredizon/go-modules"
+)
+
+func main() {
+	fmt.Println(m.SumAll(1, 2, 3, 4, 5, 6, 7))
+}
